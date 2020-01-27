e56f9bbf75a5b3d703ce2b7e622ffc5d09fa3e6d
Sampaul <38540262+sampaulp@users.noreply.github.com>
2020-01-27 16:21:45 +0100

    Create cal_timestep.py

---
+++ cal_timestep.py
@@ -0,0 +1,12 @@
+# time step calculation
+import math as m
+l = 2.0       # mesh_size in mm
+e = 210000.0    # youngs modulus N/mm2
+ro = 7.89e-09 # density tonne/mm3
+
+
+c = m.sqrt(e/ro)
+#print c
+delT = l/c
+
+print "time step =", delT, "seconds"
